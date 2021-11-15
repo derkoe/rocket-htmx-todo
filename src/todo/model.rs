92de65c0a0bdd50fad09dc1bc64868b6a8596243
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-11-15 22:19:51 +0100

    Finished adding of items

--- src/todo/model.rs
+++ src/todo/model.rs
@@ -1,7 +1,8 @@
 use crate::db::Conn;
 use crate::schema::todos;
 use crate::schema::todos::dsl::todos as all_todos;
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{NaiveDateTime, Utc};
+use diesel::result::Error;
 use diesel::{self, prelude::*, result::QueryResult};
 use diesel::{Insertable, Queryable};
 use rocket::serde::Serialize;
@@ -30,7 +31,8 @@ impl Todo {
         })
         .await
     }
-    pub async fn insert(title: String, conn: &Conn) -> QueryResult<usize> {
+
+    pub async fn insert(title: String, conn: &Conn) -> Result<Todo, Error> {
         conn.run(|c| {
             let t = Todo {
                 id: Uuid::new_v4(),
@@ -38,8 +40,21 @@ impl Todo {
                 completed: false,
                 created_timestamp: Utc::now().naive_utc(),
             };
-            diesel::insert_into(todos::table).values(&t).execute(c)
+            match diesel::insert_into(todos::table).values(&t).execute(c) {
+                Ok(_) => Result::Ok(t),
+                Err(e) => Result::Err(e),
+            }
         })
         .await
+    }
+
+    pub async fn delete(id: Uuid, conn: &Conn) -> Result<(), Error> {
+        Result::Ok(())
+        // conn.run(|c| {
+        // match diesel::delete(todos.filter(id.eq(id))).execute(&conn) {
+        //     Ok() => Result::Ok(),
+        //     Err(e) => Result::Err(e),
+        // }
+        // })
     }
 }
--- src/todo/web.rs
+++ src/todo/web.rs
@@ -1,9 +1,31 @@
 use crate::db::Conn;
 use crate::todo::model::*;
 use rocket::form::Form;
+use rocket::http::Status;
+use rocket::request::FromParam;
+use rocket::request::FromRequest;
+use rocket::request::Outcome;
+use rocket::request::Request;
 use rocket::response::Redirect;
 use rocket_dyn_templates::Template;
 use std::collections::HashMap;
+use uuid::Uuid;
+
+struct HxRequest(bool);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for HxRequest {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> rocket::request::Outcome<Self, Self::Error> {
+        let hx_request = request.headers().get_one("HX-Request");
+        if hx_request.is_some() {
+            Outcome::Success(HxRequest(true))
+        } else {
+            Outcome::Success(HxRequest(false))
+        }
+    }
+}
 
 #[get("/")]
 async fn index(conn: Conn) -> Template {
@@ -13,11 +35,40 @@ async fn index(conn: Conn) -> Template {
     Template::render("index", context)
 }
 
+#[derive(Debug, Responder)]
+pub enum TemplateOrRedirect {
+    Template(Template),
+    Redirect(Redirect),
+}
+
 #[post("/", data = "<todo>")]
-async fn add(todo: Form<NewTodo>, conn: Conn) -> Redirect {
+async fn add(
+    todo: Form<NewTodo>,
+    hx_request: HxRequest,
+    conn: Conn,
+) -> Result<TemplateOrRedirect, Status> {
     match Todo::insert(todo.into_inner().title, &conn).await {
-        Ok(_) => Redirect::found("/todos"),
-        Err(e) => Redirect::found("/"),
+        Ok(todo) => {
+            if hx_request.0 {
+                let mut context = HashMap::new();
+                context.insert("todo", todo);
+                Result::Ok(TemplateOrRedirect::Template(Template::render(
+                    "todos/item",
+                    context,
+                )))
+            } else {
+                Result::Ok(TemplateOrRedirect::Redirect(Redirect::found("/todos")))
+            }
+        }
+        Err(e) => Result::Err(Status::default()),
+    }
+}
+
+#[post("/<id>/delete")]
+async fn delete(id: Uuid, conn: Conn) -> Result<Redirect, Status> {
+    match Todo::delete(id, &conn).await {
+        Ok(_) => Result::Ok(Redirect::to(uri!("/todos"))),
+        Err(e) => Result::Err(Status::default()),
     }
 }
 
